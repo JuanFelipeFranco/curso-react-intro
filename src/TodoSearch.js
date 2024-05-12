e605013373bdd59305113ce8a819d91f59266b8e
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-05-12 00:12:02 -0500

    [ADD EVENT onClick y onChange]

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -3,7 +3,13 @@ function TodoSearch(){
     return(
         <input 
         placeholder="Cortar cebolla"
-        className='TodoSearch'
+        className="TodoSearch"
+        onChange={(event) => {
+            console.log('Escribiste en el TodoSearch');
+            console.log(event);
+            console.log(event.target);
+            console.log(event.target.value);
+          }}
         />
     );
 }
