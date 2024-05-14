5f279b3d05446ff5918e09e0c6c204e505991006
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-05-14 15:35:58 -0500

    [Loading TODO]
    agregamos nuestro cargando donde al porincipio va tener una animacion creada en el css. y despues si carga nos trae las tareas, despues si hay error nos sale error   y si no hay tareas va pedir que la cree

---
+++ src/TodosLoading/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './TodosLoading.css'
+
+function TodosLoading(){
+    return(
+      <div className="loadingProcess"></div>
+      //   <div className='LoadingTodo-container'>
+      //   <span
+      //   className='LoadingTodo-completeIcon'>
+      //   </span>
+      //   <p className='LoadingTodo-text'></p>
+      //   <span
+      //   className='LoadinTodo-deleteIcon'></span>
+      // </div>
+    );
+};
+
+export {TodosLoading };
