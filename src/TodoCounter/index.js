30242f2607fec8e020b26e97517af37f321d014a
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-05-14 15:28:01 -0500

    [Modify todo counter]
    Modificamos el todo counter que cuando hayamos completado todas las tareas nos salga " felicitaciones has completado todas las tareas"

--- src/TodoCounter/index.js
+++ src/TodoCounter/index.js
@@ -1,9 +1,18 @@
 import './TodoCounter.css';
 
 function TodoCounter({ total, completed }) {
+  let message;
+  if (total === 0) {
+    message = "No hay tareas pendientes.";
+  } else if (total === completed) {
+    message = "¡Felicidades, has completado todos los TODOs!";
+  } else {
+    message = `Has completado ${completed} de ${total} TODOs`;
+  }
+
   return (
     <h1 className="TodoCounter">
-      Has completado <span>{completed}</span> de <span>{total}</span> TODOs
+      {message}
     </h1>
   );
 }
